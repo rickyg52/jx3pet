17c59c2a9d9d92f220f8d6452648d829e20e722f
Jingyi Gao <faygao52@gmail.com>
2016-12-26 23:28:52 +1000

    lost end

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -1,18 +1,19 @@
 module Api
  	module V1
-    	# create jobs and return HTTP response
-    	class PetsController < Mjolnir::Api::ApiController
+  	# create jobs and return HTTP response
+  	class PetsController < Mjolnir::Api::ApiController
 
-      	def create
-      	end
+    	def create
+    	end
 
-      	def index
-      	end
+    	def index
+    	end
 
-      	def update
-      	end
+    	def update
+    	end
 
-      	def show
-      	end
+    	def show
+    	end
     end
+  end
 end
